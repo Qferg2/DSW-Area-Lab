12037922e197533e403d9799e25a375bda344d07
Qferg2 <54326424+Qferg2@users.noreply.github.com>
2019-10-15 09:07:46 -0700

    Update AreaLabUnfinished.py

--- AreaLabUnfinished.py
+++ AreaLabUnfinished.py
@@ -1,26 +1,29 @@
-import math 
+import math #math library has pre-written math functions
 import unittest
 
 def circleArea(radius): 
-    return 1
+    """Return the area of a circle"""
+    return math.pi * radius*radius
 
 def rectArea(base, height):
-    return 1
+    """Return the area of a rectangle"""
+    return base*height
 
 def trapArea(base1, base2, height):
-    return 1
+    """return the area of a trapezoid"""
+    return (base1+base2)/2*height
 
 def triArea(base,height):
-    return 1
+    """return the area of a triangle"""
+    return (base*height)/2
     
 	
 class MyTest(unittest.TestCase):
     def testCircleArea(self):
-    
+        self.assertEqual(circleArea(5), 25*math.pi)
     def testRectArea(self):
-    
+        self.assertEqual(rectArea(5,4), 20)
     def testTrapArea(self):
-    
+        self.assertEqual(trapArea(5,3,5), 20)
     def testTriArea(self):
-    
-    
+        self.assertEqual(triArea(5,4), 10)
